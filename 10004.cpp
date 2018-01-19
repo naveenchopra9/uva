cbe92ae8c470ca58c5bd4037e892aa54acc2a5c1
naveenchopra9 <naveenchopra9331@gmail.com>
2018-01-19 23:58:57 +0530

    navdfgdeen

--- 10004.cpp
+++ 10004.cpp
@@ -1,23 +1,24 @@
 //prakash edited
+//naveen  ....
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-/*#include <numeric>
+#include <numeric>
 #include <sstream>
 #include <fstream>
 #include <cassert>
-#include <climits>*/
+#include <climits>
 #include <cstdlib>
 #include <cstring>
 //#include <string>
 #include <cstdio>
 #include <vector>
 #include <cmath>
-#include <queue>
-#include <deque>
+// #include <queue>
+// #include <deque>
 #include <stack>
 #include <list>
-#include <map>
+// #include <map>
 #include <set>
 #include <bits/stdc++.h>
 using namespace std;
