a7ba3310f654ecf7794d6af020cb43e49e9c7d3b
Naveen Chopra <31899453+naveenchopra9@users.noreply.github.com>
2018-01-26 11:20:40 +0530

    Create 458.cpp

---
+++ 458.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+#define IN ({int n; scanf("%d", &n);    n;})
+#define CN ({char _char; scanf(" %c",&_char); _char;})a
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define FR freopen("/Users/naveen/Desktop/uva/uva/input.txt","r",stdin)
+#define FW freopen("/Users/naveen/Desktop/uva/uva/output.txt","w",stdout)
+int main()
+{
+    //FR;
+    //FW;
+    string str;
+    //cin.ignore();
+    while(getline(cin,str))
+    {
+    for(int i =0 ; i<str.length() ; i++)
+    {
+        printf("%c",str[i]-7);
+    }
+        printf("\n");
+
+        str.clear();
+    }
+  
+    return 0;
+}
+
