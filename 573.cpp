63881616f33d54ecdb318c99275a3959ecd61751
Naveen Chopra <31899453+naveenchopra9@users.noreply.github.com>
2018-02-02 02:22:23 +0530

    Create 573.cpp

---
+++ 573.cpp
@@ -0,0 +1,106 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+#include <map>
+#include <stdlib.h>
+#include <vector>
+#include <cstring>
+#include <sstream>
+using namespace std;
+//const int INF = 0x3f3f3f3f;
+#define MAXV 2147483647
+#define MINV -2147483647
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector< ii > vii;
+//typedef set<int> si;
+typedef map<int, int> msi;
+#define IN ({int n; scanf("%d", &n);    n;})
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+#define f first
+#define s second
+#define pb push_back
+#define p push
+#define SIZE 100001;
+#define sz(a) int((a).size())
+#define all(c) (c).begin(),(c).end()
+#define IN ({int n; scanf("%d", &n);    n;})
+#define CN ({char _char; scanf(" %c",&_char); _char;})
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define FR freopen("/Users/naveen/Desktop/uva/uva/input.txt","r",stdin)
+#define FW freopen("/Users/naveen/Desktop/uva/uva/output.txt","w",stdout)
+#define NIL -1
+#define Set(a, s) memset(a, s, sizeof (a))
+
+int main()
+{
+//    FR;
+//    FW;
+    double H,D,U,F;
+   while(scanf("%lf %lf %lf %lf", &H, &U, &D, &F)==4 && H!=0.0)
+   {
+       //printf("fdg");
+       double ffactor= (F/100.0)*U;
+       double inch=0.0;
+       int day=0;
+       while(1)
+       {
+           day++;
+           if(U>0)
+               inch=inch+U;
+           U=U-ffactor;
+           if(inch>H)
+           {
+               break;
+           }
+           inch=inch-D;
+           if(inch<0)
+               break;
+       }
+       if(inch<0)
+           printf("failure on day %d\n",day);
+       else
+           printf("success on day %d\n",day);
+       //cout<<"gjfdslg"<<endl;
+   }
+    // cout<<endl;
+	    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
