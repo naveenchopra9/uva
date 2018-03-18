0b5e0ddb2c0f2f11b50f18a9a7207e3638d6c483
Naveen Chopra <31899453+naveenchopra9@users.noreply.github.com>
2018-03-18 20:02:55 +0530

    Create 10104.cpp

---
+++ 10104.cpp
@@ -0,0 +1,77 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include<iomanip>
+#include<fstream>
+#include<cctype>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+#include <map>
+#include <stdlib.h>
+#include <vector>
+#include <cstring>
+#include <sstream>
+using namespace std;
+//const int INF = 0x3f3f3f3f;
+#define MAXV 2147483647
+#define MINV -2147483647
+//typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector< ii > vii;
+//typedef set<int> si;
+typedef map<int, int> msi;
+#define f first
+#define s second
+#define pb push_back
+#define p push
+#define SIZE 100001;
+#define sz(a) int((a).size())
+#define all(c) (c).begin(),(c).end()
+#define IN ({int n; scanf("%d", &n);    n;})
+#define CN ({char _char; scanf(" %c",&_char); _char;})
+#define REP(i, a, b) for (int i = int(a); i < int(b); i++)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define TRvi(c, it) for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+#define FR freopen("/Users/naveen/Desktop/uva/uva/input.txt","r",stdin)
+#define FW freopen("/Users/naveen/Desktop/uva/uva/output.txt","w",stdout)
+#define NIL -1
+#define Set(a, s) memset(a, s, sizeof (a))
+long long int X,Y,D;
+void Euclid_problem(long long int A,long long int B)
+{
+    if(B==0)
+    {
+        D=A;
+        X=1;
+        Y=0;
+    }
+    else
+    {
+        Euclid_problem(B, A%B);
+        long long int temp=X;
+        X=Y;
+        Y=temp-(A/B)*Y;
+    }
+        
+}
+int main()
+{
+//        FR;
+//        FW;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long int A,B;
+    while(cin>>A>>B)
+    {
+    Euclid_problem(A,B);
+    cout<<X<<" "<<Y<<" "<<D<<endl;
+    }
+    return 0;
+}
